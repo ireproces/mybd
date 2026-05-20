e8f3960cb4141388e1e2167eff24219d75a72b41
Irene Proietti Cesarini <ireneproietticesarini@gmail.com>
2026-05-20 20:57:27 +0200

    Updates the docker container startup script

--- Flight-Delay_Project/scripts/run_local.sh
+++ Flight-Delay_Project/scripts/run_local.sh
@@ -3,18 +3,19 @@
 # Enable script stop if a command fails
 set -e
 
-# Colors for readable output
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+# colors for readable output
+BLUE='\033[0;34m'
+NC='\033[0m'
 
 # Move the execution to the root folder of the project
 cd "$(dirname "$0")/.."
 
-echo -e "${GREEN}[1/2] Preparing the Docker environment (bigdata-env)...${NC}"
+echo -e "${BLUE}[1/2] Preparing the Docker environment (bigdata-env)...${NC}"
 # Builds the image. Uses the cache if the layers haven't changed.
 docker build -t bigdata-env .
 
-echo -e "${GREEN}[2/2] Starting the container and mounting the working directory...${NC}"
+echo -e "\\n${BLUE}[2/2] Starting the container and mounting the working directory...${NC}"
+echo ""
 echo "You are now in the isolated environment. Run your scripts from here."
 echo "To exit, type: exit"
 
